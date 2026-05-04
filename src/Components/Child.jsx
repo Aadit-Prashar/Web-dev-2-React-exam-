9b14da5acd499796cf9fd4c5cc68659ae972b7b0
aaditprashar <prasharaadit@gmail.com>
2026-05-04 10:50:16 +0530

    Updated

--- src/Components/Child.jsx
+++ src/Components/Child.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Child = ({name,age}) => {
   return (
     <>
-    <h2>Rendered in child</h2>
+    <h2>Props passed to Child</h2>
     <h1>Name:{name}</h1>
     <h1>Age:{age}</h1>
     </>
